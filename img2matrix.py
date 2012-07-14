72076f2ef9220881fbe2a86b47db19ade3661989
Philip Mallory <philip@pmallory.com>
2012-07-14 14:55:43 -0400

    make the output file's name more descriptive.

--- img2matrix.py
+++ img2matrix.py
@@ -11,5 +11,5 @@ if __name__ == '__main__':
 
     img_array = misc.imread(img_path)
     
-    save(os.path.splitext(img_path)[0], img_array)
+    save(os.path.splitext(img_path)[0]+'-matrix', img_array)
 
